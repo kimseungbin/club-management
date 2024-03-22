c1379b4b10e070b03e5ab82a90ce27068f89f471
SeungBin Kim <luxorienskim@gmail.com>
2024-03-22 16:32:48 +0900

    feat: implement typeorm connection using config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule, ConfigType } from '@nestjs/config'
+import databaseConfig from './config/database.config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 
 @Module({
-	imports: [],
+	imports: [ConfigModule.forRoot({
+		load: [databaseConfig],
+	}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule.forFeature(databaseConfig)],
+			inject: [databaseConfig.KEY],
+			useFactory: async (databaseConfiguration: ConfigType<typeof databaseConfig>) => ({
+				type: 'postgres',
+				host: databaseConfiguration.host,
+				port: databaseConfiguration.port,
+				username: databaseConfiguration.username,
+				password: databaseConfiguration.password,
+				database: databaseConfiguration.database,
+				entities: ['dist/**/*.entity.js'],
+				synchronize: true,
+				namingStrategy: new SnakeNamingStrategy(),
+			}),
+		})],
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/config/database.config.ts
@@ -0,0 +1,11 @@
+import { registerAs } from '@nestjs/config'
+import * as process from 'process'
+
+export default registerAs('database', () => ({
+	host: process.env.DB_HOST || 'database',
+	port: +process.env.DB_PORT || 5432,
+	database: process.env.DB_NAME || 'postgres',
+	username: process.env.DB_USERNAME || 'postgres',
+	password: process.env.DB_PASSWORD || 'postgres',
+
+}))
