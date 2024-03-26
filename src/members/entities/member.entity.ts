b82213bbe7ec748e47aea4d551de0b16120077da
SeungBin Kim <luxorienskim@gmail.com>
2024-03-26 10:00:54 +0900

    feat: update Member entity to inherit from AbstractEntity

--- src/members/entities/member.entity.ts
+++ src/members/entities/member.entity.ts
@@ -1,7 +1,8 @@
 import { Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { AbstractEntity } from '../../common/entities/abstract.entity'
 
 @Entity('members')
-export class Member {
+export class Member extends AbstractEntity {
 	@PrimaryGeneratedColumn('uuid')
 	id: string
 }
