e5d3eef4d43e8307ea9e5ba7db9db3d9a099f606
SeungBin Kim <luxorienskim@gmail.com>
2024-04-04 13:47:13 +0900

    feat: update all APIs but health and static page to have prefix /api in their path

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { AllExceptionsFilter } from './common/filters/all-exceptions/all-exceptions.filter'
+import { RequestMethod } from '@nestjs/common'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const configService = app.get(ConfigService)
 	const generalConfig = configService.get('general')
 	app.useGlobalFilters(new AllExceptionsFilter())
+
+	// Set all routes to start with prefix of /api but health and statically served page.
+	app.setGlobalPrefix('api', {
+		exclude: [
+			{ path: 'health', method: RequestMethod.GET },
+			{ path: '/', method: RequestMethod.GET },
+		],
+	})
+
 	await app.listen(generalConfig.port)
 }
+
 bootstrap()
