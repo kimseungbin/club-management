9e1fac90df0457865e8bc66b80bf82d8a91dfed2
SeungBin Kim <luxorienskim@gmail.com>
2024-04-09 20:12:10 +0900

    feat: add phone column in member entity

--- src/members/entities/member.entity.ts
+++ src/members/entities/member.entity.ts
@@ -1,8 +1,13 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 import { AbstractEntity } from '../../common/entities/abstract.entity'
+import { Matches } from 'class-validator'
 
 @Entity('members')
 export class Member extends AbstractEntity {
 	@PrimaryGeneratedColumn('uuid')
 	id: string
+
+	@Column({ unique: true })
+	@Matches(/^010(\d{7,8})$/)
+	phone: string
 }
