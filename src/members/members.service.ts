a9821898d9ad15e1c4d07a0d83c1506f0a579928
SeungBin Kim <luxorienskim@gmail.com>
2024-04-09 20:14:15 +0900

    feat: add create member error handling

--- src/members/members.service.ts
+++ src/members/members.service.ts
@@ -1,14 +1,24 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { CreateMemberDto } from './dto/create-member.dto'
 import { UpdateMemberDto } from './dto/update-member.dto'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Member } from './entities/member.entity'
 import { Repository } from 'typeorm'
 import { NoContentException } from '../common/exceptions/no-content.exception'
+import { DuplicateItemException } from '../common/exceptions/duplicate-item.exception'
+import databaseConfig from '../configs/database.config'
+import { ConfigType } from '@nestjs/config'
+
+interface DatabaseError extends Error {
+	code?: string
+	detail?: string
+}
 
 @Injectable()
 export class MembersService {
 	constructor(
+		@Inject(databaseConfig.KEY)
+		private readonly databaseConfiguration: ConfigType<typeof databaseConfig>,
 		@InjectRepository(Member)
 		private readonly memberRepository: Repository<Member>,
 	) {
@@ -17,7 +27,12 @@ export class MembersService {
 	async create(dto: CreateMemberDto) {
 		const member = this.memberRepository.create(dto)
 
-		return await this.memberRepository.save(member)
+		try {
+			return await this.memberRepository.save(member)
+		} catch (e) {
+			console.dir(e)
+			this.handleExceptions(e)
+		}
 	}
 
 	async findAll() {
@@ -39,5 +54,34 @@ export class MembersService {
 
 	remove(id: number) {
 		return `This action removes a #${id} member`
+	}
+
+	private handleExceptions(error: DatabaseError) {
+		if (this.databaseConfiguration.type === 'postgres') {
+			console.log(error)
+			// todo don't use magic string
+			if (error.code === '23505') {
+				const { duplicateField, value } = this.extractDuplicateFieldAndValue(error.detail)
+				throw new DuplicateItemException(Member.name, duplicateField, value)
+			}
+			// Todo refactor using custom exception
+			throw new Error(`An unexpected error occurred while using Postgres: ${error.message}`)
+		}
+		throw new Error('A database error occurred, and the error handling for the current DBMS type is not implemented yet')
+	}
+
+	private extractDuplicateFieldAndValue(message: string) {
+		const re = /Key \((.+)\)=\((.+)\) already exists/
+		const match = message.match(re)
+
+		if (match) {
+			return {
+				duplicateField: match[1],
+				value: match[2],
+			}
+		}
+
+		// Todo refactor using custom exception
+		throw new Error('Could not match the pattern')
 	}
 }
