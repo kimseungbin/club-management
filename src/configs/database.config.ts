34f9a36af8371f5f107a3d654b2ba0881f5978cf
SeungBin Kim <luxorienskim@gmail.com>
2024-04-09 20:12:35 +0900

    feat: add database type env var

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -2,6 +2,7 @@ import { registerAs } from '@nestjs/config'
 import * as process from 'process'
 
 export default registerAs('database', () => ({
+	type: process.env.DB_TYPE || 'postgres',
 	host: process.env.DB_HOST || 'database',
 	port: +process.env.DB_PORT || 5432,
 	database: process.env.DB_NAME || 'postgres',
